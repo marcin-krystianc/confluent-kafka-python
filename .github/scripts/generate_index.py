7e1f689c6db03b9882e0cab6d5c9cfb4e0e94912
Marcin Krystianc <marcin.krystianc@gmail.com>
2025-02-25 16:44:44 +0100

    encode urls

--- .github/scripts/generate_index.py
+++ .github/scripts/generate_index.py
@@ -1,12 +1,14 @@
 import os
 import sys
 import re
-from pathlib import Path
-from github import Github
-from typing import List, Dict
 import itertools
 import requests
 import hashlib
+
+from urllib.parse import quote
+from pathlib import Path
+from github import Github
+from typing import List, Dict
 
 HTML_TEMPLATE = """<!DOCTYPE html>
  <html>
@@ -27,7 +29,7 @@ def normalize(name):
 def calculate_sha256(file_path):
     with open(file_path, "rb") as f:
         digest = hashlib.file_digest(f, "sha256")
-
+    
     return digest.hexdigest()
 
 class PackageIndexBuilder:
@@ -94,7 +96,7 @@ class PackageIndexBuilder:
                             f.write(chunk)
 
                 sha256_hash = calculate_sha256(package_dir / filename)
-                file_links.append(f'<a href="{filename}#sha256={sha256_hash}">{filename}</a><br/>')
+                file_links.append(f'<a href="{quote(filename)}#sha256={sha256_hash}">{filename}</a><br/>')
 
             package_index = HTML_TEMPLATE.format(
                 package_name=f"Links for {package}",
